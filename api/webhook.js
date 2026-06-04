399311babb37fc3d85286d4d2fc374fecf78101d
PRIYANKA-Nigam <nigampriyanka042.com>
2026-06-04 05:47:58 +0530

    test auto deploy

--- api/webhook.js
+++ api/webhook.js
@@ -33,6 +33,7 @@ const AMOUNT_MAP = {
    WEBHOOK HANDLER
 ========================= */
 export default async function handler(req, res) {
+  console.log("GitHub auto deploy test");
   try {
     console.log("🔔 WEBHOOK HIT");
 
